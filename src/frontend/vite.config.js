81e5abf752bd6f4bf6b7e7ac2aa2fd35c1fcf7f7
obaodelana <obaloluwaode@gmail.com>
2025-06-03 00:56:14 -0600

    Switched to local backend

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -3,17 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/sonar': {
-        target: 'https://goodsline.onrender.com',
-        changeOrigin: true,
-      },
-      '/stats': {
-        target: 'https://goodsline.onrender.com',
-        changeOrigin: true,
-      },
-    }
-  }
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/sonar': {
+				target: 'http://127.0.0.1:5000',
+				changeOrigin: true,
+			},
+			'/stats': {
+				target: 'http://127.0.0.1:5000',
+				changeOrigin: true,
+			},
+		}
+	}
 })
